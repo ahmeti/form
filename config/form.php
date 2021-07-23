613e8d890279c2872477a254b1e3325acf0c87e5
Ahmet Imamoglu <ahmet363@gmail.com>
2021-07-23 18:59:48 +0300

    config/form güncellendi.

--- config/form.php
+++ config/form.php
@@ -3,20 +3,33 @@
 return [
 
     'form' => [
-        'action' => null,
         'autocomplete' => 'off',
         'class' => null,
         'enctype' => null,
         'method' => 'GET',
-        'target' => null,
 
         'data_form_type' => 'ajax',
 
         'onkeypress' => 'App.stopEnterSubmitting(window.event)',
     ],
 
-    'input_text' => [
+    'template' =>
+        '<div class="form-group@group_class@"@group_style@>'.
+            '<label class="col-sm-3 control-label">'.
+                '<div class="ellipsis">'.
+                    '<span data-toggle="tooltip" title="@label_title@">@label@</span>'.
+                '</div>'.
+            '</label>'.
+            '<div class="col-sm-9 error-message">@element@@description_template@</div>'.
+        '</div>',
+
+    'description_template' => '<p class="help-block">@description@</p>',
+
+    'input' => [
         'autocomplete' => 'off',
+    ],
+
+    'input_text' => [
         'class' => 'form-control input-sm',
     ]
 
